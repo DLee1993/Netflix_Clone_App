010124d95ce86fcff9be42b0f966de2fab3a9995
DLee1993 <Dai180293@gmail.com>
2023-03-09 15:18:32 +0000

    landing page/hero content completed

--- client/src/components/partials/HeroContent.js
+++ client/src/components/partials/HeroContent.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { IoIosArrowForward } from "react-icons/io";
+
 const HeroContent = () => {
-    return <section className='hero_content'></section>;
+    const [registerValue, setRegisterValue] = useState("johnDoe@example.com");
+
+    const registerUser = (e) => {
+        console.log("submitted");
+    };
+
+    return (
+        <section className='hero_content'>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <p className='supHeading'>Watch anywhere. Cancel at any time.</p>
+            <p className='subHeading'>
+                Ready to watch? Enter your email to create or restart your membership.
+            </p>
+            <section className='membership_fields'>
+                <label htmlFor='membership_register' placeholder='email' />
+                <input
+                    className='register_input'
+                    type='email'
+                    name='membership_register'
+                    autoComplete='email'
+                    max-length='50'
+                    value={registerValue}
+                    readOnly
+                    placeholder='Email Address'
+                />
+                <button onClick={registerUser} className='register_button'>
+                    Get Started <IoIosArrowForward />
+                </button>
+            </section>
+            <sub>Please use the provided email address to simulate registering as a user</sub>
+        </section>
+    );
 };
 
 export default HeroContent;
