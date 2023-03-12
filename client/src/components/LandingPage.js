cf237b20b6a31de0842551def07161146e62db8d
DLee1993 <Dai180293@gmail.com>
2023-03-12 17:02:38 +0000

    removed all unused styling and jsx

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,23 +1,14 @@
 import LandingPageNav from "./partials/LandingPageNav";
 import HeroContent from "./partials/HeroContent";
-import Promotions from "./partials/Promotions";
-import { WatchDevices, StreamDevices, Profiles, Downloads } from "./partials/Cards";
 
 const LandingPage = () => {
     return (
         <main>
             <section className='background-image-container '>
-                <Promotions />
                 <section className='landingPageHero'>
                     <LandingPageNav />
                     <HeroContent />
                 </section>
-            </section>
-            <section className='story-cards membership_benefits'>
-                <WatchDevices />
-                <StreamDevices />
-                <Profiles />
-                <Downloads />
             </section>
         </main>
     );
